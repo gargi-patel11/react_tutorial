c608d44914a780dafd2f680247dad560b3044f11
gargi-patel11 <gargipatel2211@gmail.com>
2025-01-25 23:50:41 +0530

    showing alerts

--- src/App.js
+++ src/App.js
@@ -3,18 +3,57 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.css"
 import Navbar from "./components/Navbar"
 import Textarea from "./components/Textarea"
-import About from "./components/About"
+// import About from "./components/About"
+import { useState } from 'react';
+import Alert from './components/Alert';
 
 
 function App() {
+  
+  let [mode , setMode]=useState('light')
+  let[alert , setalert]=useState(null)
+
+  const createalert = (message , type)=>{
+    setalert(
+      {
+        message :message,
+        type : type
+      }
+    )
+    setTimeout(() => {
+      setalert(null)
+    }, 1500);
+  }
+    
+
+  const darkmodeenalbe =()=>{
+    if(mode==='light'){
+    setMode('dark')
+    document.body.style.backgroundColor="rgb(8 35 62)"
+    createalert("dark mode enable" ,"dark")
+    
+
+
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor="white"
+      createalert("dark mode disable" ,"success")
+
+    }
+
+
+  }
   return (
     <>
-    <Navbar />
+
+    <Navbar mode={mode} darkmode={darkmodeenalbe}/>
+    <Alert alert={alert}/>
     <div className="container">
-    <Textarea heading="add your text here" />
-    <About ></About>
-  </div>    
-    </>
+    <Textarea heading="add your text here" mode={mode} createalert ={createalert}/>
+    {/* <About /> */}
+  </div> 
+      </>
   );
 }
 
--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -1,37 +1,102 @@
 import React, { useState } from "react";
+// import props.createalertert from './Alert'
+
 
 export default function Textarea(props) {
   const hendleUPClicked = () => {
     setText(text.toUpperCase());
+    if(props.mode==="dark")
+      {
+        props.createalert(
+          "converted to UpperCase " , "dark"
+        )
+      }
+      else{
+    props.createalert(
+      "converted to UpperCase " , "success"
+    )
+  }
   };
   const hendledwClicked = () => {
     setText(text.toLowerCase());
+    if(props.mode==="dark")
+      {
+    props.createalert(
+      "converted to LowerCase " , "dark"
+    )
+  }
+  else{
+    props.createalert(
+      "converted to LowerCase " , "success"
+    )
+  }
+  
   };
 
   const onchangefun = (event) => {
     setText(event.target.value);
+   
   };
   const copytext = () => {
     let text22 = document.getElementById("exampleFormControlTextarea1");
     text22.select();
     navigator.clipboard.writeText(text22.value);
+    if(props.mode==="dark")
+    {
+      props.createalert(
+        "text copy to clipBoard " , "dark"
+      )
+    }
+      else{
+      props.createalert(
+        "text copy to clipBoard " , "success"
+      )
+    }
+    
+   
   };
   const removeextraspaces = () => {
     let find = text.split(/[" "]+/)
     setText(find.join(" "))
-    console.log("hello im here")
+    if(props.mode === "dark")
+    {
+    props.createalert(
+      "removed extra spaces " , "dark"
+    )
+  }
+  else{
+    props.createalert(
+      "removed extra spaces " , "success"
+    )
+  }
+    
   };
+  const hendlecleartext =()=>{
+    setText("");
+    if(props.mode === "dark")
+      {
+        props.createalert(
+          "text cleared" , "dark"
+        )
+      }
+      else{
+    props.createalert(
+      "text cleared" , "success"
+    )
+  }
+  }
 
 
   const [text, setText] = useState("");
   return (
     <>
       <div>
-        <h1>{props.heading}</h1>
+        <h1 className= {`text-${props.mode === 'dark' ? 'white' :'dark'}`}>{props.heading}</h1>
 
-        <div className="form-group textarea ">
+        <div className="form-group textarea" style={{backgroundColor:(props.mode==="dark" ? "rgb(8 35 62)" : "white" ), color:(props.mode==="light" ? "rgb(8 35 62)" : "white") }}>
           <textarea
-            className="form-control"
+            className= {`form-control text-${props.mode === 'dark' ? 'white' :'dark'}  `}
+            style={{backgroundColor:(props.mode==="dark" ? "rgb(8 35 62)" : "white") }}
             id="exampleFormControlTextarea1"
             rows="9"
             placeholder="enter your text here"
@@ -59,17 +124,23 @@ export default function Textarea(props) {
           >
             remove extra spaces 
           </button>
+          <button
+            className="btn btn-primary my-3 mx-3"
+            onClick={hendlecleartext}
+          >
+            clear text
+          </button>
         </div>
       </div>
       <div className="container">
-        <h3>text summary </h3>
-        <p>
+        <h3 className= {`text-${props.mode === 'dark' ? 'white' :'dark'}`}>text summary </h3>
+        <p className= {`text-${props.mode === 'dark' ? 'white' :'dark'}`}>
           {text.split(" ").length} words and {text.length} characters
         </p>
       </div>
       <div>
-        <h3>text preview</h3>
-        <p>{text}</p>
+        <h3 className= {`text-${props.mode === 'dark' ? 'white' :'dark'}`}>text preview</h3>
+        <p className= {`text-${props.mode === 'dark' ? 'white' :'dark'}`}>{text}</p>
       </div>
     </>
   );
